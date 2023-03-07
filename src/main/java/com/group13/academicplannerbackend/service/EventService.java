7685c5a07ca8837f029d6b6ccb86fed03b43b8f7
Meet Sinojia <mt845464@dal.ca>
2023-03-06 20:32:58 -0400

    Add service for creating events and repeated events

---
+++ src/main/java/com/group13/academicplannerbackend/service/EventService.java
@@ -0,0 +1,7 @@
+package com.group13.academicplannerbackend.service;
+
+import com.group13.academicplannerbackend.model.Event;
+
+public interface EventService {
+    void createEvent(Event event);
+}
