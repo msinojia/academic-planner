e22bfd244ae10589291e7a7cb96fd500d63929dc
Meet Sinojia <mt845464@dal.ca>
2023-03-20 20:02:59 -0300

    Front-end code to check if user is authenticated before calling restricted APIs using JWT

--- academic-planner-frontend/src/pages/profileSetup/api.js
+++ academic-planner-frontend/src/pages/profileSetup/api.js
@@ -1,5 +1,6 @@
-import axios from '../../axiosConfig';
+// import axios from '../../axiosConfig';
+import { authApi } from '../../axiosConfig';
 
 export const addEvent = async (data) => {
-  return await axios.post('/event', data);
+  return await authApi.post('/event', data);
 };
